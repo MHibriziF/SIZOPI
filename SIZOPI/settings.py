5b52778c7d14ce299a9fcd33b7a8bbdf663fed5e
anthef <anthonyef09@gmail.com>
2025-04-24 22:25:16 +0700

    add instaled apps

--- SIZOPI/settings.py
+++ SIZOPI/settings.py
@@ -50,6 +50,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'blue',
+    'green',
+    'yellow',
+    'red'
 ]
 
 MIDDLEWARE = [
