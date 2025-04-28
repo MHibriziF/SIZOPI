5a3a2dfade6f74f7d059b3979918f8f4d70d6145
MHibriziF <hibrizifarghana@gmail.com>
2025-04-28 15:12:53 +0700

    setup views and path

--- blue/views.py
+++ blue/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def reservasi(request):
+    return render(request, 'reservasi.html')
+
+def kelola_wahana(request):
+    if 'admin' not in request.session["roles"] :
+        return redirect('main:dashboard')
+    return render(request, 'wahana.html')
+
+def kelola_atraksi(request):
+    if 'admin' not in request.session["roles"] :
+        return redirect('main:dashboard')
+    
+    # data_atraksi = execute_query(
+    #     """
+        
+    #     """,
+        
+    # )
+    
+    
+    return render(request, 'atraksi.html')
+
+def kelola_pengunjung(request):
+    return render(request, 'kelola_pengunjung.html')
+
